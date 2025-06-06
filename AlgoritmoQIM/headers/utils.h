e6ccfbd86382d178bf86411ea80e05afa333ee95
Pvbb <Pablo0gf7@uma.es>
2025-06-07 00:14:03 +0200

    Unificacion en un solo proyecto

--- AlgoritmoQIM/headers/utils.h
+++
@@ -1,7 +0,0 @@
-#ifndef UTILS_H
-#define UTILS_H
-#include <sndfile.h>
-void print_usage();
-void calculate_mse_psnr(const char *original_file, const char *stego_file, const char *output_txt, sf_count_t samples_to_check);
-char *read_text_file(const char *filename);
-#endif // UTILS_H
